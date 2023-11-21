cbef2f2d12aff981d7dae289392bcdf92e4bb6d3
Your Name <you@example.com>
2023-11-21 11:49:07 +1100

    start!

---
+++ optical_flow/optical_flow.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def lucas_kanade(init_img, target_img):
+    result = np.zeros((init_img.shape, init_img.shape, 2))
+    return result
+
+
+if __name__ == "__main__":
+    pass
